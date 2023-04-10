dd61b20f2b0bd5ac8de2be2a55651938819698db
alejo <alejo200003@gmail.com>
2023-04-10 06:25:50 -0300

    SE AGREGARON LOS DATOS DE DOCENTES

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3000;
+const alumnosRouter = require('./routes/alumnos.route')
+const aularRouter = require('./routes/aulas.routes')
+
+app.use(express.json())
+app.use('/alumnos', alumnosRouter.router)
+app.use('/aulas', aularRouter.router )
+
+app.listen(PORT, ()=>{console.log(`App lista escuhando en el puerto ${PORT}`)} )
+
